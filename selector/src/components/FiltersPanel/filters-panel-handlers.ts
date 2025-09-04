5268ee8d3b63cd4660a57bbf12bf435672c3cbed
John Feng <john.feng@intel.com>
2025-09-04 13:42:13 +0800

    rm selectors

--- selector/src/components/FiltersPanel/filters-panel-handlers.ts
+++
@@ -1,9 +0,0 @@
-const filtersOpenedClassName = 'filters-opened';
-
-export const openFiltersPanel = (): void => {
-  document.body.classList.add(filtersOpenedClassName);
-};
-
-export const closeFiltersPanel = (): void => {
-  document.body.classList.remove(filtersOpenedClassName);
-};
--- selector/src/shared/constants.js
+++
@@ -1,4 +0,0 @@
-// @ts-check
-
-export const NOTEBOOKS_METADATA_FILE_NAME = 'notebooks-metadata-map.json';
-export const NOTEBOOKS_STATUS_FILE_NAME = 'notebooks-status-map.json';
