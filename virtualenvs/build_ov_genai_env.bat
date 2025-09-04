930f5e04a9182ec2055a063a3519ce47924d2642
John Feng <john.feng@intel.com>
2025-09-04 13:35:54 +0800

    Update content for AI Campus (#2)
    * add virtual env for differnet ai catagory
    
    * ignore genai env
    
    * remove requirements and update LLM notebooks for course
    
    * remove requirements and update t2i notebooks for course
    
    * ignore gradio temporary files
    
    * remove requirements and update hello npu notebooks for course
    
    * remove requirements and update yolov8 notebooks for course
    
    * add utils
    
    * ignore model files

---
+++ virtualenvs/build_ov_genai_env.bat
@@ -0,0 +1,20 @@
+:<<BATCH
+    @echo off
+    echo **** OpenVINO GenAI Environment Setup started **** 
+    python -m pip install virtualenv | find /V "already satisfied"
+    python -m venv openvino_genai_env
+    echo ---- activating python venv   -----------------------------------------------------------------
+    call "openvino_genai_env\Scripts\activate"
+    python -m pip install --upgrade pip
+    pip install -r "requirements.txt"
+    pip install "openvino>=2025.2.0" "openvino-tokenizers>=2025.2.0" "openvino-genai>=2025.2.0"
+    pip install "transformers>=4.45,<4.50" --extra-index-url https://download.pytorch.org/whl/cpu
+    pip install "optimum-intel[nncf]>=1.25.2" --extra-index-url https://download.pytorch.org/whl/cpu
+    pip install "huggingface-hub>=0.26.5" "datasets<4.0.0" "einops" "transformers_stream_generator"
+    pip install "accelerate" "gradio>=4.19" "tiktoken" "bitsandbytes"
+    pip install Pillow "diffusers>=0.30.3" "gradio>=4.19" "typing_extensions>=4.9" "tqdm"
+    echo ---- deactivating python venv -----------------------------------------------------------------
+    call deactivate
+    echo **** OpenVINO GenAI Environment Setup completed ****
+    exit /b
+BATCH
