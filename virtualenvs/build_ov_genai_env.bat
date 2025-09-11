87f5b22bc900e5c60a415ab617f69070796ef099
John Feng <john.feng@intel.com>
2025-09-11 14:42:33 +0800

    add mcp envs

--- virtualenvs/build_ov_genai_env.bat
+++ virtualenvs/build_ov_genai_env.bat
@@ -12,7 +12,9 @@
     pip install "optimum-intel[nncf]>=1.25.2" --extra-index-url https://download.pytorch.org/whl/cpu
     pip install "huggingface-hub>=0.26.5" "datasets<4.0.0" "einops" "transformers_stream_generator"
     pip install "accelerate" "gradio>=4.19" "tiktoken" "bitsandbytes"
-    pip install Pillow "diffusers>=0.30.3" "gradio>=4.19" "typing_extensions>=4.9" "tqdm"
+    pip install "Pillow" "diffusers>=0.30.3" "gradio>=4.19" "typing_extensions>=4.9" "tqdm"
+    pip install "mcp-server-time" "mcp-server-fetch" 
+    pip install "pydantic==2.9.2" "pydantic-core==2.23.4" "modelscope_studio==1.0.0-beta.8"
     echo ---- deactivating python venv -----------------------------------------------------------------
     call deactivate
     echo **** OpenVINO GenAI Environment Setup completed ****
