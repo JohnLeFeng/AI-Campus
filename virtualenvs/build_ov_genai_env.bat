35095ee109146d98967ef67b893d6143adab3e2b
John Feng <john.feng@intel.com>
2025-09-12 11:00:02 +0800

    remove pydanti from construct script

--- virtualenvs/build_ov_genai_env.bat
+++ virtualenvs/build_ov_genai_env.bat
@@ -13,8 +13,7 @@
     pip install "huggingface-hub>=0.26.5" "datasets<4.0.0" "einops" "transformers_stream_generator"
     pip install "accelerate" "gradio>=4.19" "tiktoken" "bitsandbytes"
     pip install "Pillow" "diffusers>=0.30.3" "gradio>=4.19" "typing_extensions>=4.9" "tqdm"
-    pip install "mcp-server-time" "mcp-server-fetch" 
-    pip install "pydantic==2.9.2" "pydantic-core==2.23.4" "modelscope_studio==1.0.0-beta.8"
+    pip install "mcp-server-time" "mcp-server-fetch" "modelscope_studio==1.0.0-beta.8"
     echo ---- deactivating python venv -----------------------------------------------------------------
     call deactivate
     echo **** OpenVINO GenAI Environment Setup completed ****
